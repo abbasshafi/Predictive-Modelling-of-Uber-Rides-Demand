50a34f18420ed37e073517f364b8805b5eed40a7
abbasshafi <abbasshafi.2000@gmail.com>
2021-11-25 09:41:05 +0500

    Update MLR.py

--- MLR.py
+++ MLR.py
@@ -5,17 +5,14 @@ import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
-data = pd.read_csv("taxi.csv")
-# print(data)
+data = pd.read_csv('/home/abbas/Projects/Uber-Rides-Prediction/taxi.csv')
+# print(data.head())
 
-x=data.iloc[:,:-1].values
-y=data.iloc[:,-1].values
-# print(x)
-# print(y)
+data_x = data.iloc[:,0:-1].values
+data_y = data.iloc[:,-1].values
+print(data_y)
 
-x_train,y_train,x_test,y_test=train_test_split(x,y,test_size=0.3,random_state=0)
+X_train,X_test,y_train,y_test = train_test_split(data_x,data_y,test_size=0.3,random_state=0)
 
-reg= LinearRegression()
-reg.fit(x_train,y_train)
-
-print("Training Score",reg.score(x_train,y_train))
+reg = LinearRegression()
+reg.fit(X_train,y_train)
